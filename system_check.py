f8751c004b36ebe858d35ff90af7bdf532537e2d
MarcosPimienta <fenix3819@gmail.com>
2023-07-31 10:57:33 -0500

    Try and Except approach for preventing app crash in case of errors

--- system_check.py
+++ system_check.py
@@ -3,9 +3,26 @@ import sys
 import platform
 import psutil
 
+try:
+    os_name = os.name
+    os_release = platform.release()
+    platform_system = platform.system()
+    platform_architecture = platform.architecture()
+except Exception as e:
+    print(f"An error occurred when trying to obtain OS information: {str(e)}")
+    sys.exit(1)
 
-os_name = os.name
-os_release = platform.release()
-platform_system = platform.system()
-platform_architecture = platform.architecture()
+try:
+    virtual_memory = psutil.virtual_memory()
+    system_uptime = psutil.boot_time()
+except Exception as e:
+    print(f"An error occurred when trying to obtain memory and uptime information: {str(e)}")
+    sys.exit(1)
 
+partitions = []
+try:
+    for partition in psutil.disk_partitions():
+        partitions.append(psutil.disk_usage(partition.mountpoint))
+except Exception as e:
+    print(f"An error occurred when trying to obtain partition information: {str(e)}")
+    sys.exit(1)
