5db1118e8feaecc7467d3f6894f347b48cee22ec
MarcosPimienta <fenix3819@gmail.com>
2023-07-31 11:09:27 -0500

    Encapsulated functionalities

--- system_check.py
+++ system_check.py
@@ -3,26 +3,32 @@ import sys
 import platform
 import psutil
 
-try:
-    os_name = os.name
-    os_release = platform.release()
-    platform_system = platform.system()
-    platform_architecture = platform.architecture()
-except Exception as e:
-    print(f"An error occurred when trying to obtain OS information: {str(e)}")
-    sys.exit(1)
 
-try:
-    virtual_memory = psutil.virtual_memory()
-    system_uptime = psutil.boot_time()
-except Exception as e:
-    print(f"An error occurred when trying to obtain memory and uptime information: {str(e)}")
-    sys.exit(1)
+def get_os_info():
+    try:
+        os_name = os.name
+        os_release = platform.release()
+        platform_system = platform.system()
+        platform_architecture = platform.architecture()
+    except Exception as e:
+        print(f"An error trying to obtain OS information: {str(e)}")
+        sys.exit(1)
 
-partitions = []
-try:
-    for partition in psutil.disk_partitions():
-        partitions.append(psutil.disk_usage(partition.mountpoint))
-except Exception as e:
-    print(f"An error occurred when trying to obtain partition information: {str(e)}")
-    sys.exit(1)
+
+def get_sys_resources():
+    try:
+        virtual_memory = psutil.virtual_memory()
+        system_uptime = psutil.boot_time()
+    except Exception as e:
+        print(f"An error trying to obtain memory and uptime information: {str(e)}")
+        sys.exit(1)
+
+def get_sys_partitions():
+
+    partitions = []
+    try:
+        for partition in psutil.disk_partitions():
+            partitions.append(psutil.disk_usage(partition.mountpoint))
+    except Exception as e:
+        print(f"An error trying to obtain partition information: {str(e)}")
+        sys.exit(1)
