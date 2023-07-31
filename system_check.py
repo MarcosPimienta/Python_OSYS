322fdce1ef1b23febf5991d944b9a6bdc1191d84
MarcosPimienta <fenix3819@gmail.com>
2023-07-31 10:38:07 -0500

    Created variables tohold os-platform related information

---
+++ system_check.py
@@ -0,0 +1,11 @@
+import os
+import sys
+import platform
+import psutil
+
+
+os_name = os.name
+os_release = platform.release()
+platform_system = platform.system()
+platform_architecture = platform.architecture()
+
